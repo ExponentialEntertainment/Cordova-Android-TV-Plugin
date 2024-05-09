65a88f03ef6b7cc1f48a0a1ff4d1d9683434e18c
Justin <justin.wright@exponentialent.com>
2024-05-09 13:30:14 -0700

    adding debugging logs

--- afterPrepare.js
+++ afterPrepare.js
@@ -1,24 +1,30 @@
 #!/usr/bin/env node
+const fs = require("fs");
+const path = require("path");
 
 module.exports = function (context) {
-	const fs = require("fs");
-	const path = require("path");
+	console.log("[ee-cordova-plugin-android-tv][afterPrepare]");
 
 	const platformRoot = path.join(context.opts.projectRoot, "platforms/android");
 	const manifestFile = path.join(platformRoot, "AndroidManifest.xml");
+	console.log({ platformRoot, manifestFile });
 
 	if (fs.existsSync(manifestFile)) {
 		fs.readFile(manifestFile, "utf8", function (err, data) {
+			console.log({ data });
 			if (err) {
+				console.error("[Unable to find AndroidManfiest.xml", JSON.stringify(err));
 				throw new Error("Unable to find AndroidManifest.xml: " + err);
 			}
 
 			if (!/<application[^>]*\bandroid:banner/.test(data)) {
-				console.log("Adding banner attribute");
+				console.log("[Adding banner attribute]");
 				data = data.replace(
 					/<application/g,
 					'<application android:banner="@drawable/banner"'
 				);
+			} else {
+				console.log("[Android:Banner already exists. Wont add.]");
 			}
 
 			fs.writeFile(manifestFile, data, "utf8", function (err) {
